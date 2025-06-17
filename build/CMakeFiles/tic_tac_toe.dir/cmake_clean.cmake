ce2ebfb96e9848171e22f8c9970319aae10da030
us3r <hajiyevraul99@gmail.com>
2025-06-17 15:47:14 +0400

    Initial commit: base project for Tic Tac Toe

---
+++ build/CMakeFiles/tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe.dir/link.d"
+  "CMakeFiles/tic_tac_toe.dir/main.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/main.cpp.o.d"
+  "tic_tac_toe"
+  "tic_tac_toe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
